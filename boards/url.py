aa4bf55df301c0a1979a4257c9ea9b99c646f106
Black Heart <developerblackheart@gmail.com>
2022-10-06 02:24:59 -0700

    used django forms and added 'new_topic' page and 'topics' page

--- boards/url.py
+++ boards/url.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('test/', views.test, name='test'),
     path('', views.home, name='home'),
     path('boards/<str:board_name>/', views.board_topics, name='board_topics'),
+    path('boards/<str:board_name>/new/', views.new_topic, name='new_topic'),
 ]
--- boards/views.py
+++ boards/views.py
@@ -1,11 +1,14 @@
 from multiprocessing import context
+from pydoc_data.topics import topics
 from unicodedata import name
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 import random
 from . import models
 # import .models
 # import models
+from . import forms
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -34,3 +37,30 @@ def board_topics(request, board_name):
     board = get_object_or_404(models.Board, name=board_name)
     context = {'board': board}
     return render(request, 'topics.html', context)
+
+
+# New Topic
+def new_topic(request, board_name):
+    
+    board = get_object_or_404(models.Board, name=board_name)
+    user = User.objects.first()
+    if request.method == "POST":
+        form = forms.NewTopicForm(request.POST)
+        if form.is_valid():
+            topic = form.save(commit=False)
+            topic.board = board
+            topic.created_by = user
+            topic.save()
+            
+            post = models.Post.objects.create(
+                comment=form.cleaned_data.get('comment'),
+                topic=topic,
+                created_by=user
+            )
+            
+            return redirect('board_topics', board_name=board.name)
+    else:
+        form = forms.NewTopicForm()
+
+    context = {'board': board, 'form': form}
+    return render(request, 'new_topic.html', context)
