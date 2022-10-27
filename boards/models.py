bc2ae7cb050d6369835883d87fcfd9c82ac17f35
Black Heart <developerblackheart@gmail.com>
2022-10-27 19:13:27 +0500

    added QuerySets 1
    - return count posts
    - return count topic
    - return last post
    - added all function on models.py in class Board as functions

--- boards/models.py
+++ boards/models.py
@@ -1,8 +1,6 @@
-from enum import unique
-from msvcrt import open_osfhandle
-from unittest.util import _MAX_LENGTH
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.text import Truncator
 # Create your models here.
 
 
@@ -13,6 +11,14 @@ class Board(models.Model):
 
     def __str__(self):
         return self.name
+    
+    # return posts count
+    def get_posts_count(self):
+        return Post.objects.filter(topic__board=self).count()
+    
+    # return last post
+    def get_last_post(self):
+        return Post.objects.filter(topic__board=self).order_by("-created_dt").first()
 
 
 # Topic Table
@@ -21,6 +27,9 @@ class Topic(models.Model):
     board = models.ForeignKey(Board, related_name="topics", on_delete=models.CASCADE)
     created_by = models.ForeignKey(User, related_name="topics", on_delete=models.CASCADE)
     created_dt = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.subject
 
 
 # Post Table
@@ -29,3 +38,8 @@ class Post(models.Model):
     topic = models.ForeignKey(Topic, related_name="posts", on_delete=models.CASCADE)
     created_by = models.ForeignKey(User, related_name="posts", on_delete=models.CASCADE)
     created_dt = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        
+        com = Truncator(self.comment)
+        return com.chars(30)
